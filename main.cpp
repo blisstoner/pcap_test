912d2c2c9b6d3b9f67ddc4de2f589869e04626f0
blisstoner <blisstoner@gmail.com>
2018-09-21 20:33:40 +0900

    modify structure of each header as defined libnet.h

--- main.cpp
+++ main.cpp
@@ -1,187 +1,141 @@
+#include <libnet.h>
 #include <pcap.h>
+#include <stdint.h>
 #include <stdio.h>
 
 const int ETHERNET_HEADER_LEN = 14;
-const int IP_HEADER_LEN = 20; // at least 20
-const int TCP_HEADER_LEN = 20; // at least 20 
-class ethernet_header{
-public:
-    u_char destMAC[6], srcMAC[6];
-    u_short Etype;
-    void printMAC(int isDest);
-};    
-void ethernet_header::printMAC(int isDest){ // 0 : source, 1 : dest
-   u_char* mac;
-   if(isDest) mac = destMAC;
-   else mac = srcMAC;
-   printf("%02x",*mac);
-   mac++;
-   int len = 5;
-   while(len--) printf(":%02x",*(mac++));
-   printf("\n");
-}
-class IPv4_header{
-public:
-    u_char Ver_IHL, DSCP_ECN;
-    u_short len, Id;
-    u_short Flag_Fragment;
-    u_char TTL, Protocol;
-    u_short checksum;
-    u_int srcIP, destIP;
-    u_char* option;
-    u_int IHL;
-    void printIP(int isDest);
-    ~IPv4_header();
-};
-void IPv4_header::printIP(int isDest){
-    u_int ip;
-    if(isDest) ip = destIP;
-    else ip = srcIP;
-    printf("%d.%d.%d.%d\n",ip>>24,((ip>>16 & 0xff)),((ip>>8)&0xff),ip&0xff);
-}
-IPv4_header::~IPv4_header(){
-    if(option) delete [] option;
-}
-class TCP_header{
-public:
-    u_short srcPort, destPort;
-    u_int seqNumber, ackNumber;
-    u_short flag, windowSize;
-    u_short checksum, urgentPtr;
-    u_char* option;
-    u_int DataOffset;
-    ~TCP_header();
-};
-TCP_header::~TCP_header(){
-   if(option) delete [] option;
-}
+const int IP_HEADER_LEN = 20;   // at least 20
+const int TCP_HEADER_LEN = 20;  // at least 20
 
-void dump(const u_char* p, int len){
-// parse Ethernet header
-    ethernet_header ethHeader;
-    if(len<ETHERNET_HEADER_LEN){
-        printf("length is not enough to parse  ehternet header.\n");
-        return;
+void print_mac(uint8_t *addr) {  // 0 : source, 1 : dest
+  for (int i = 0; i < 6; i++) {
+    printf("%02x", *(addr++));
+    if (i != 5) printf(":");
+  }
+  printf("\n");
+}
+void print_ip(uint32_t ip) {
+  printf("%d.%d.%d.%d\n", ip >> 24, ((ip >> 16 & 0xff)), ((ip >> 8) & 0xff),
+         ip & 0xff);
+}
+void dump(const uint8_t *p, int len) {
+  // parse Ethernet header
+  libnet_ethernet_hdr ethHeader;
+  if (len < ETHERNET_HEADER_LEN) {
+    printf("length is not enough to parse ehternet header.\n");
+    return;
+  }
+  for (int i = 0; i < 6; i++) ethHeader.ether_dhost[i] = (uint8_t) * (p++);
+  for (int i = 0; i < 6; i++) ethHeader.ether_shost[i] = (uint8_t) * (p++);
+  ethHeader.ether_type = ((uint16_t) * (p++)) << 8;
+  ethHeader.ether_type |= (uint16_t) * (p++);
+  printf("src  MAC Address : ");
+  print_mac(ethHeader.ether_shost);
+  printf("dest  MAC Address : ");
+  print_mac(ethHeader.ether_dhost);
+  if (ethHeader.ether_type != ETHERTYPE_IP) return;
+  // parse IP header
+  if (len < ETHERNET_HEADER_LEN + IP_HEADER_LEN) {
+    printf("length is not enough to parse ip header.\n");
+    return;
+  }
+  libnet_ipv4_hdr ipHeader;
+  ipHeader.ip_v = (uint8_t)((*p) >> 4);
+  ipHeader.ip_hl = (uint8_t)((*(p++) & 0xf));
+  ipHeader.ip_tos = (uint8_t) * (p++);
+  ipHeader.ip_len = ((uint16_t) * (p++)) << 8;
+  ipHeader.ip_len |= (uint16_t) * (p++);
+  if (ETHERNET_HEADER_LEN + ipHeader.ip_len > len) {
+    printf("length is not enough to parse ip header.\n");
+    return;
+  }
+  ipHeader.ip_id = ((uint16_t) * (p++)) << 8;
+  ipHeader.ip_id |= (uint16_t) * (p++);
+  ipHeader.ip_off = ((uint16_t) * (p++)) << 8;
+  ipHeader.ip_off |= (uint16_t) * (p++);
+  ipHeader.ip_ttl = (uint8_t) * (p++);
+  ipHeader.ip_p = (uint8_t) * (p++);
+  ipHeader.ip_sum = ((uint16_t) * (p++)) << 8;
+  ipHeader.ip_sum |= (uint16_t) * (p++);
+  ipHeader.ip_src.s_addr = ((uint32_t) * (p++)) << 24;
+  ipHeader.ip_src.s_addr |= ((uint32_t) * (p++)) << 16;
+  ipHeader.ip_src.s_addr |= ((uint32_t) * (p++)) << 8;
+  ipHeader.ip_src.s_addr |= (uint32_t) * (p++);
+  ipHeader.ip_dst.s_addr = ((uint32_t) * (p++)) << 24;
+  ipHeader.ip_dst.s_addr |= ((uint32_t) * (p++)) << 16;
+  ipHeader.ip_dst.s_addr |= ((uint32_t) * (p++)) << 8;
+  ipHeader.ip_dst.s_addr |= (uint32_t) * (p++);
+  if (ipHeader.ip_hl < 5) {
+    printf("wrong IHL value(%d) in IP header\n", ipHeader.ip_hl);
+    return;
+  }
+  if (ipHeader.ip_hl > 5) {
+    uint32_t option_len = (ipHeader.ip_hl - 5) << 2;
+    if (len < ETHERNET_HEADER_LEN + option_len) {
+      printf("length is not enough to parse ip header.\n");
+      return;
     }
-    for(int i = 0; i < 6; i++) ethHeader.destMAC[i] = (u_char) *(p++);
-    for(int i = 0; i < 6; i++) ethHeader.srcMAC[i] = (u_char) *(p++);
-    ethHeader.Etype = ((u_short) *(p++)) << 8;
-    ethHeader.Etype |= (u_short) *(p++);
-    printf("src  MAC Address : ");
-    ethHeader.printMAC(0);
-    printf("dest  MAC Address : ");
-    ethHeader.printMAC(1);
-    if(ethHeader.Etype != 0x0800) return;
-    // parse IP header
-    if(len < ETHERNET_HEADER_LEN+IP_HEADER_LEN){
-        printf("length is not enough to parse ip header.\n");
-        return;
+    uint8_t ip_option[option_len];
+    for (int i = 0; i < option_len; i++) ip_option[i] = (uint8_t) * (p++);
+  }
+  printf("src IP : ");
+  print_ip(ipHeader.ip_src.s_addr);
+  printf("dest IP : ");
+  print_ip(ipHeader.ip_dst.s_addr);
+  if (ipHeader.ip_p != IPPROTO_TCP) return;
+  // parse TCP header
+  if (len < ETHERNET_HEADER_LEN + ((int)ipHeader.ip_hl << 2) + TCP_HEADER_LEN) {
+    printf("length is not enough to parse tcp header.\n");
+    return;
+  }
+  libnet_tcp_hdr tcpHeader;
+  tcpHeader.th_sport = ((uint16_t) * (p++)) << 8;
+  tcpHeader.th_sport |= (uint16_t) * (p++);
+  tcpHeader.th_dport = ((uint16_t) * (p++)) << 8;
+  tcpHeader.th_dport |= (uint16_t) * (p++);
+  tcpHeader.th_seq = ((uint32_t) * (p++)) << 24;
+  tcpHeader.th_seq |= ((uint32_t) * (p++)) << 16;
+  tcpHeader.th_seq |= ((uint32_t) * (p++)) << 8;
+  tcpHeader.th_seq |= (uint32_t) * (p++);
+  tcpHeader.th_ack = ((uint32_t) * (p++)) << 24;
+  tcpHeader.th_ack |= ((uint32_t) * (p++)) << 16;
+  tcpHeader.th_ack |= ((uint32_t) * (p++)) << 8;
+  tcpHeader.th_ack |= (uint32_t) * (p++);
+  tcpHeader.th_off = (uint8_t)((*p) >> 4);
+  tcpHeader.th_x2 = (uint8_t)((*(p++)) & 0xf);
+  tcpHeader.th_flags = (uint8_t) * (p++);
+  tcpHeader.th_win = ((uint16_t) * (p++)) << 8;
+  tcpHeader.th_win |= (uint16_t) * (p++);
+  tcpHeader.th_sum = ((uint16_t) * (p++)) << 8;
+  tcpHeader.th_sum |= (uint16_t) * (p++);
+  tcpHeader.th_urp = ((uint16_t) * (p++)) << 8;
+  tcpHeader.th_urp |= (uint16_t) * (p++);
+  if (tcpHeader.th_off < 5) {
+    printf("wrong DataOffset value(%d) in TCP header\n", tcpHeader.th_off);
+    return;
+  }
+  if (tcpHeader.th_off > 5) {
+    if (len < ETHERNET_HEADER_LEN + ((int)ipHeader.ip_hl << 2) +
+                  ((int)tcpHeader.th_off << 2)) {
+      printf("length is not enough to parse tcp header.\n");
+      return;
     }
-    IPv4_header ipHeader;
-    ipHeader.option = NULL;
-    ipHeader.Ver_IHL = (u_char) *(p++);
-    ipHeader.DSCP_ECN = (u_char) *(p++);
-    ipHeader.len = ((u_short) *(p++)) << 8;
-    ipHeader.len |= (u_short) *(p++);
-    if(ETHERNET_HEADER_LEN + ipHeader.len > len){
-        printf("length is not enough to parse ip header.\n");
-        return;
-    }
-    ipHeader.Id = ((u_short) *(p++)) << 8;
-    ipHeader.Id |= (u_short) *(p++);
-    ipHeader.Flag_Fragment =  ((u_short) *(p++)) << 8;
-    ipHeader.Flag_Fragment |= (u_short) *(p++);
-    ipHeader.TTL = (u_char) *(p++);
-    ipHeader.Protocol = (u_char) *(p++);
-    ipHeader.checksum =  ((u_short) *(p++)) << 8;
-    ipHeader.checksum |= (u_short) *(p++);
-    ipHeader.srcIP =  ((u_int) *(p++)) << 24;
-    ipHeader.srcIP |=  ((u_int) *(p++)) << 16;
-    ipHeader.srcIP |=  ((u_int) *(p++)) << 8;
-    ipHeader.srcIP |=  (u_int) *(p++);
-    ipHeader.destIP =  ((u_int) *(p++)) << 24;
-    ipHeader.destIP |=  ((u_int) *(p++)) << 16;
-    ipHeader.destIP |=  ((u_int) *(p++)) << 8;
-    ipHeader.destIP |=  (u_int) *(p++);
-    ipHeader.IHL = (ipHeader.Ver_IHL&0xf);
-    if(ipHeader.IHL < 5){
-        printf("wrong IHL value(%d) in IP header\n", ipHeader.IHL);
-        return;
-    }
-    else if(ipHeader.IHL == 5){
-        ipHeader.option = NULL;
-    }
-    else{
-        if(len < ETHERNET_HEADER_LEN+(ipHeader.IHL<<2)){
-            printf("ipHeader.IHL : %d\n",ipHeader.IHL);
-            printf("length is not enough to parse ip header.\n");
-            return;
-        
-        }
-        ipHeader.option = new u_char[(ipHeader.IHL-5)<<2];
-        for(int i = 0; i < ((ipHeader.IHL-5)<<2); i++)
-            ipHeader.option[i] = (u_char) *(p++);
-    }
-    printf("src IP : ");
-    ipHeader.printIP(0);
-    printf("dest IP : ");
-    ipHeader.printIP(1);
-    if(ipHeader.Protocol != 0x06) return;
-    // parse TCP header
-    if(len < ETHERNET_HEADER_LEN+(ipHeader.IHL<<2)+TCP_HEADER_LEN){
-        printf("length is not enough to parse tcp header.\n");
-        return;
-    }
-    TCP_header tcpHeader;
-    tcpHeader.option = NULL;
-    tcpHeader.srcPort =  ((u_short) *(p++)) << 8;
-    tcpHeader.srcPort |=  (u_short) *(p++);
-    tcpHeader.destPort =  ((u_short) *(p++)) << 8;
-    tcpHeader.destPort |=  (u_short) *(p++);
-    tcpHeader.seqNumber =  ((u_int) *(p++)) << 24;
-    tcpHeader.seqNumber |=  ((u_int) *(p++)) << 16;
-    tcpHeader.seqNumber |=  ((u_int) *(p++)) << 8;
-    tcpHeader.seqNumber |=  (u_int) *(p++);
-    tcpHeader.ackNumber =  ((u_int) *(p++)) << 24;
-    tcpHeader.ackNumber |=  ((u_int) *(p++)) << 16;
-    tcpHeader.ackNumber |=  ((u_int) *(p++)) << 8;
-    tcpHeader.ackNumber |=  (u_int) *(p++);
-    tcpHeader.flag = ((u_short) *(p++)) << 8;
-    tcpHeader.flag |= (u_short) *(p++);
-    tcpHeader.DataOffset = tcpHeader.flag >> 12;
-    tcpHeader.windowSize =  ((u_short) *(p++)) << 8;
-    tcpHeader.windowSize |=  (u_short) *(p++);
-    tcpHeader.checksum =  ((u_short) *(p++)) << 8;
-    tcpHeader.checksum |=  (u_short) *(p++);
-    tcpHeader.urgentPtr =  ((u_short) *(p++)) << 8;
-    tcpHeader.urgentPtr |=  (u_short) *(p++);
-    if(tcpHeader.DataOffset < 5){
-        printf("wrong DataOffset value(%d) in TCP header\n",tcpHeader.DataOffset);
-        return;
-    }        
-    else if(tcpHeader.DataOffset == 5){
-        tcpHeader.option = NULL;
-    }
-    else{
-        if(len < ETHERNET_HEADER_LEN+(ipHeader.IHL<<2)+(tcpHeader.DataOffset<<2)){
-            printf("length is not enough to parse tcp header.\n");
-            return;
-        }
-        tcpHeader.option = new u_char[((tcpHeader.DataOffset-5)<<2)];
-        for(int i = 0; i < ((tcpHeader.DataOffset-5)<<2); i++)
-            tcpHeader.option[i] = (u_char) *(p++);
-    }
-    printf("src Port : %d\n",tcpHeader.srcPort);
-    printf("dest Port : %d\n",tcpHeader.destPort);
-    int dataLen = ipHeader.len - (ipHeader.IHL<<2) - (tcpHeader.DataOffset<<2);
-    if(dataLen == 0) return;
-    printf("---------hex data---------\n");
-    int printlen = dataLen > 32 ? 32 : dataLen;
-    for(int i = 0; i < printlen; i++){
-        printf("%02x ", *(p++));
-        if(i==15) printf("\n");
-    }
-    printf("\n");
+    uint32_t option_len = (tcpHeader.th_off - 5) << 2;
+    uint8_t tcp_option[option_len];
+    for (int i = 0; i < option_len; i++) tcp_option[i] = (uint8_t) * (p++);
+  }
+  printf("src Port : %d\n", tcpHeader.th_sport);
+  printf("dest Port : %d\n", tcpHeader.th_dport);
+  int dataLen = ipHeader.ip_len - ((int)ipHeader.ip_hl << 2) -
+                ((int)tcpHeader.th_off << 2);
+  if (dataLen == 0) return;
+  printf("---------hex data---------\n");
+  int printlen = dataLen > 32 ? 32 : dataLen;
+  for (int i = 0; i < printlen; i++) {
+    printf("%02x ", *(p++));
+    if (i == 15) printf("\n");
+  }
+  printf("\n");
 }
 
 void usage() {
@@ -189,23 +143,24 @@ void usage() {
   printf("sample: pcap_test wlan0\n");
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   if (argc != 2) {
     usage();
     return -1;
   }
 
-  char* dev = argv[1];
+  char *dev = argv[1];
   char errbuf[PCAP_ERRBUF_SIZE];
   pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+  //pcap_t *handle = pcap_open_offline("tcp-port-80-test.gilgil.pcap", errbuf);
   if (handle == NULL) {
     fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
     return -1;
   }
 
   while (1) {
-    struct pcap_pkthdr* header;
-    const u_char* packet;
+    struct pcap_pkthdr *header;
+    const uint8_t *packet;
     int res = pcap_next_ex(handle, &header, &packet);
     if (res == 0) continue;
     if (res == -1 || res == -2) break;
